a16944f913094c7520346341c718d9a48a82e72c
Mark Torres <mark.torres.mets@gmail.com>
2017-04-03 19:29:57 -0500

    Buttons with icons

--- app/src/main/java/com/hsoft/androidwebbrowser/MainActivity.java
+++ app/src/main/java/com/hsoft/androidwebbrowser/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hsoft.androidwebbrowser;
 
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,13 +10,14 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 	private WebView webView;
 	private EditText textUrl;
-	private Button btnGo;
+	private String homeUrl;
 
 	// CLICK LISTENERS
 
@@ -26,19 +28,67 @@ public class MainActivity extends AppCompatActivity {
 		}
 	};
 
+	private View.OnClickListener clickBack = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			clickBtnBack();
+		}
+	};
+
+	private View.OnClickListener clickForward = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			clickBtnForward();
+		}
+	};
+
+	private View.OnClickListener clickHome = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			clickBtnHome();
+		}
+	};
+
+	private View.OnClickListener clickRefresh = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			clickBtnRefresh();
+		}
+	};
+
+	// ACTIVITY - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		homeUrl = "http://urdevel.mtorres.urstaging.com/dump.php";
+
 		textUrl = (EditText) findViewById(R.id.text_url);
-		btnGo = (Button) findViewById(R.id.btn_go);
+		Button btnGo = (Button) findViewById(R.id.btn_go);
+		ImageButton btnBack = (ImageButton) findViewById(R.id.btn_back);
+		ImageButton btnForward = (ImageButton) findViewById(R.id.btn_forward);
+		ImageButton btnHome = (ImageButton) findViewById(R.id.btn_home);
+		ImageButton btnRefresh = (ImageButton) findViewById(R.id.btn_refresh);
+
 
 		btnGo.setOnClickListener(clickGo);
+		btnBack.setOnClickListener(clickBack);
+		btnForward.setOnClickListener(clickForward);
+		btnHome.setOnClickListener(clickHome);
+		btnRefresh.setOnClickListener(clickRefresh);
 
 		webView = (WebView) findViewById(R.id.web_view);
-		webView.setWebViewClient(new WebViewClient());
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				super.onPageStarted(view, url, favicon);
+				textUrl.setText(url);
+			}
+		});
 
+		// configure web view
 		WebSettings webSettings = webView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 	}
@@ -57,5 +107,25 @@ public class MainActivity extends AppCompatActivity {
 			return;
 		}
 		webView.loadUrl(urlString);
+	}
+
+	private void clickBtnBack() {
+		if (webView.canGoBack()) {
+			webView.goBack();
+		}
+	}
+
+	private void clickBtnForward() {
+		if (webView.canGoForward()) {
+			webView.goForward();
+		}
+	}
+
+	private void clickBtnHome() {
+		webView.loadUrl(homeUrl);
+	}
+
+	private void clickBtnRefresh() {
+		webView.reload();
 	}
 }
